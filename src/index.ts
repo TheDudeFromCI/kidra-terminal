e33f9bb002ae363e91cb6f9db5dc0cfca7bfaf96
TheDudeFromCI <thedudefromci@gmail.com>
2023-08-29 00:58:16 -0700

    Removed unused event.
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { Bot } from 'mineflayer'
 import { UserInterface } from './UserInterface'
-import { Command } from './Commands'
 import { pathfinder as pathfinderPlugin } from 'mineflayer-pathfinder'
 
 declare module 'mineflayer' {
   interface Bot {
     terminal: UserInterface
-  }
-  interface BotEvents {
-    'command': (cmd: Command) => void
   }
 }
 
